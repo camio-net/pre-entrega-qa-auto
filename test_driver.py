06cb9361ecf2dfa68b1edb786e9f2ed29d78330c
Damian <damianmartinezrx@gmail.com>
2025-10-15 10:21:10 -0300

    Actualización del proyecto

--- test_driver.py
+++ test_driver.py
@@ -11,10 +11,17 @@ try:
     driver.find_element(By.ID,"password").send_keys("secret_sauce")
     driver.find_element(By.ID,"login-button").click()
     
+    #validadcion de producto
+    assert  '/inventory.html' in driver.current_url
 
-    print(driver.title)
-    assert driver.title == "Swag Labs"
-
-    time.sleep(5)
+    #interacciones
+    producto = driver.find_elements(By.CLASS_NAME,"inventory_item")
+    producto [0].find_element(By.TAG_NAME,"button").click()
+    carrito = driver.find_element(By.CLASS_NAME,"shopping_cart_container").text
+    assert carrito == "1"
+    
+    
+    print("Ingreso a la pagina de navegacion, OK")
+    time.sleep(2)
 finally:
     driver.quit()
