040a515929abb7e2ad98da8bb36ba05fc557ab8f
Schumyd Damian <schumyd.d@gmail.com>
2025-10-15 15:43:50 -0300

    primeras modificacion del programa para ejecutarse en pytest

--- test_login.py
+++ test_login.py
@@ -2,26 +2,38 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 import time
 
-driver = webdriver.Chrome() 
-driver.implicitly_wait(5)
+#Validacion de credenciales
+def test_login():
+    driver = webdriver.Chrome() 
+    driver.implicitly_wait(5)
 
-try: 
+    try: 
     
-    driver.get("https://www.saucedemo.com")
-    
-    #Validacion de credenciales
+        driver.get("https://www.saucedemo.com")   
+        time.sleep(1)
+        driver.find_element(By.ID,"user-name").send_keys("standard_user")
+        driver.find_element(By.ID,"password").send_keys("secret_sauce")
+        driver.find_element(By.ID,"login-button").click()
+        print ("Validacion de credecion exitosa, OK")
 
-    driver.find_element(By.ID,"user-name").send_keys("standard_user")
-    driver.find_element(By.ID,"password").send_keys("secret_sauce")
-    driver.find_element(By.ID,"login-button").click()
+        time.sleep(2)
+        
+        #validadcion de pagina inventario inventory.html
+        assert  '/inventory.html' in driver.current_url, "No se direccion correctamente al inventario"
+        print ("Validacion de 'pagina invenory.html', ok")
+
+    except Exception as e:
+        print(f"Error en test login {e}")
+        raise 
+
+    finally:
+        driver.quit()
+
     
-    
-    print ("Validacion de credecion exitosa, OK")
 
 
-    #validadcion de pagina inventario inventory.html
-    assert  '/inventory.html' in driver.current_url
-    print ("Validacion de 'pagina invenory.html', ok")
+    
+    
 
     #interacciones
     '''
@@ -29,7 +41,7 @@ try:
     producto [0].find_element(By.TAG_NAME,"button").click()
     carrito = driver.find_element(By.CLASS_NAME,"shopping_cart_container").text
     assert carrito == "1"
-    '''
+    
 
     #Validacion Pagina producto
     producto = driver.find_elements(By.CLASS_NAME,"inventory_item")
@@ -46,3 +58,4 @@ try:
     time.sleep(2)
 finally:
     driver.quit()
+'''
