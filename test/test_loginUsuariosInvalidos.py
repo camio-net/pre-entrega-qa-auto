4aad42d21e48fd067b4e57ac31863b31041f4380
Schumyd Damian <schumyd.d@gmail.com>
2025-11-03 15:17:13 -0300

    se agrego test_loginInvalidos.py

--- test/test_loginUsuariosInvalidos.py
+++ test/test_loginUsuariosInvalidos.py
@@ -1,17 +1,18 @@
 from selenium.webdriver.common.by import By
-from selenium import webdriver 
+from selenium import webdriver
 import pytest
 
 from utils.datos import leer_csv_login
-from pages.loginPage import LoginPage
+from pages.loginPage import login_page
 
-leer_csv_login("datos/datos_usuarios_invalidos.csv")
-def test_login_validation(login_page,usuario,password,debe_funcionar):
+
+@pytest.mark.parametrize("usuario, password, debe_funcionar", leer_csv_login("datos/datos_usuarioInvalido.csv"))
+def test_login_validation(login_page, usuario, password, debe_funcionar):
     driver = login_page
+
     print(debe_funcionar)
     if debe_funcionar == 'True':
         assert "/inventory.html" in driver.current_url, "No se redirgio al inventario"
     elif debe_funcionar == 'False':
-        mensaje_error = LoginPage(driver).obtener_error()
+        mensaje_error = login_page(driver).obtener_error()
         assert "Epic sadface" in mensaje_error, "el mensaje de error no se esta mostrando"
-
